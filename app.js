a7e635be5b2781156ecd60252cf6a8a0f2890b57
weishare <weishare@outlook.com>
2013-10-19 14:14:58 +0800

    dev

--- app.js
+++ app.js
@@ -11,15 +11,18 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -27,7 +30,15 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/latest', routes.latest);
+app.post('/morelatest', routes.morelatest);
 app.post('/', routes.start);
+
+app.get('/messages/:id', routes.show);
+app.get('/like/:id', routes.like);
+
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@
 var weixin = require('weixin-api');
 var mongoose = require('mongoose');
 
+
+var MYHOST = 'http://63.223.73.213/';
+var LATEST = 'http://63.223.73.213/latest';
+var LATESTQ = 'http://63.223.73.213/latest/?UserName=';
+
 mongoose.connect('mongodb://localhost/WXMessage');
 
 var db = mongoose.connection;
@@ -13,28 +18,46 @@ db.once('open',function(){
     console.log('open WXMessage');
 });
 
+var Schema = mongoose.Schema;
 
-var WXMessageSchema = mongoose.Schema({
+
+var WXMessageSchema = Schema({
         toUserName: String,
         fromUserName: String,
         createTime: Number,
+        likes: [String],
+        fucks: [String],
         picUrl: String,
         msgId: String,
-        content: String
+        content: String,
+        comments: [{ body: String, date: Date }],
+        title: {type: String, default: '无标题'},
+        description: {type: String, default: ''}
     });
 
 
 WXMessageSchema.methods.findSelf = function(cb){
-    this.model('message').find({fromUserName:this.fromUserName}).limit(10).sort({'_id':-1}).exec(cb);
+    this.model('message').find({fromUserName:this.fromUserName}).limit(9).sort({'_id':-1}).exec(cb);
 }
 
-WXMessageSchema.statics.findNew = function(cb){
-    this.find().limit(10).sort({'_id':-1}).exec(cb);
+WXMessageSchema.statics.findNew = function(num, cb){
+
+    this.find().limit(num).sort({'_id':-1}).exec(cb);
+}
+
+WXMessageSchema.statics.findNewByLast = function(num, last, cb){
+    this.find().where('_id').lt(last).limit(num).sort({'_id':-1}).exec(cb);
 }
 
 
 var WXMessageModel = mongoose.model("message", WXMessageSchema);
 
+
+Date.prototype.toMyformat = function(){
+    var m = this.getMonth();
+    m++;
+    return this.getFullYear() + '-' + m + '-' + this.getDate();
+}
 
 // config
 weixin.token = 'honghong';
@@ -49,15 +72,22 @@ weixin.textMsg(function(msg) {
 
     switch (msg.content) {
         case "n" :
-            WXMessageModel.findNew(function(err, messages){
+            WXMessageModel.findNew(9, function(err, messages){
                 var articles = [];
                 for (var i = 0; i < messages.length; i++) {
                     articles[i] = {
-                        title : i,
-                        description : "最新暴照",
+                        title : messages[i].title,
+                        description : messages[i].description,
                         picUrl : messages[i].picUrl,
                         url : messages[i].picUrl
                     };
+                };
+
+                articles[messages.length] = {
+                    title : '更多',
+                    description : "更多最新",
+                    picUrl : '/images/icon_arrow_more.png',
+                    url : LATESTQ + msg.fromUserName
                 };
 
                 resMsg = {
@@ -77,8 +107,8 @@ weixin.textMsg(function(msg) {
                 var articles = [];
                 for (var i = 0; i < messages.length; i++) {
                     articles[i] = {
-                        title : i,
-                        description : "最新暴照",
+                        title : messages[i].title,
+                        description : messages[i].description,
                         picUrl : messages[i].picUrl,
                         url : messages[i].picUrl
                     };
@@ -146,5 +176,118 @@ exports.index = function(req, res){
 exports.start = function(req, res){
 	weixin.loop(req, res);
 };
+
+
+exports.latest = function(req, res){
+    console.log(req.query)
+    if (req.query.UserName) {
+        res.cookie('UserName', req.query.UserName);
+    };
+    
+    WXMessageModel.findNew(20, function(err, messages){
+
+        res.render('latest', {
+            messages: messages,
+            first: true
+        });
+    });
+
+};
+
+
+exports.morelatest = function(req, res){
+    if (req.body._id) {
+        WXMessageModel.findNewByLast(20, req.body._id, function(err, messages){
+            res.render('messages', {
+                messages: messages,
+                first: false
+            });
+        });
+    } else {
+        res.send(200, 'error');
+    }
+};
+
+
+exports.show = function(req, res){
+    if(req.params.id){
+        WXMessageModel.findById(req.params.id, function(err, obj){
+            if (err) {
+                console.log(err);
+                res.send(200, 'fail');
+                return;
+            };
+
+            console.log(obj)
+
+            console.log(req.cookies)
+
+            obj.likeDisabled = obj.likes.indexOf(req.cookies.UserName) === -1 ? "" : "disabled";
+            obj.fuckDisabled = obj.fucks.indexOf(req.cookies.UserName) === -1 ? "" : "disabled";
+            res.render('show', obj);
+        })
+    }
+};
+
+
+
+exports.like = function(req, res){
+    if(req.params.id){
+
+        var conditions  = { _id: req.params.id},
+        update = { $addToSet: { likes:  req.cookies.UserName} };
+        WXMessageModel.update(conditions, update, function(err){
+            if (err) console.log(err);
+
+            res.send(200, 'success');
+        })
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
